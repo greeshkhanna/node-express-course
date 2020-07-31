88955fd51eac8fecf80f20d8b824ef8b97baf306
Mayank Khanna <khanna98@outlook.com>
2020-07-31 13:07:14 +0530

    Added First GET route

--- server.js
+++ server.js
@@ -1,7 +1,29 @@
-const express = require('express')
-const chalk = require('chalk')
+const express = require('express') // For making an express Server
+const chalk = require('chalk') // For Better looking logs
+
+// Create a new Express Server
 const app = express()
 
+// Mock Data to send as a response from Express Server
+const mockUserData = [
+    {
+        name: "Mayank"
+    },
+    {
+        name: "Greesh"
+    }
+]
+
+// Fetch all users
+app.get('/users', (req, res) => {
+    res.json({
+        success: true,
+        message: "Successfully fetched the users",
+        users: mockUserData
+    })
+})
+
+// Start listening to server at Port 8080
 app.listen(8080, () => {
     console.log(chalk.green("Server is running on http://localhost:8080"))
 })
